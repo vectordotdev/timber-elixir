a8bfa10eebc8ced15c36fa58c29096475142057f
Ben Johnson <bjohnson@binarylogic.com>
2016-11-24 01:45:17 -0600

    Bump version to 0.4.4

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Timber.Mixfile do
 
   @source_url "https://github.com/timberio/timber-elixir"
   @homepage_url "https://github.com/timberio/timber-elixir"
-  @version "0.4.3"
+  @version "0.4.4"
 
   # Project manifest for Mix
   #
