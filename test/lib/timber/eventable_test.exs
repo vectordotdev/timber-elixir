c2263083c41ea2dcd11d872d89bfff46a3dfe827
Ben Johnson <bjohnson@binarylogic.com>
2017-02-17 15:55:19 -0600

    Support root key maps for events and contexts

---
+++ test/lib/timber/eventable_test.exs
@@ -0,0 +1,23 @@
+defmodule Timber.EventableTest do
+  use Timber.TestCase
+
+  alias Timber.Eventable
+
+  describe "Timber.Eventable.to_event/1" do
+    test "map with a single root key" do
+      event = Eventable.to_event(%{build: %{version: "1.0.0"}})
+      assert event == %Timber.Events.CustomEvent{data: %{version: "1.0.0"}, type: :build}
+    end
+
+    test "map with multiple root keys" do
+      assert_raise FunctionClauseError, fn ->
+        Eventable.to_event(%{build: %{version: "1.0.0"}, another: 1})
+      end
+    end
+
+    test "structured map" do
+      event = Eventable.to_event(%{type: :build, data: %{version: "1.0.0"}})
+      assert event == %Timber.Events.CustomEvent{data: %{version: "1.0.0"}, type: :build}
+    end
+  end
+end
