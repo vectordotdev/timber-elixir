300fbd4de204efacac4da13cc77345023c568aa5
Ben Johnson <bjohnson@binarylogic.com>
2017-02-17 16:11:35 -0600

    Update @timber.io to @metadata (#75)

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,7 +1,7 @@
 defmodule Timber.TestHelpers do
   def parse_log_line(line) do
     IO.puts("Splitting #{line}")
-    split_string = String.split(line, "@timber.io")
+    split_string = String.split(line, "@metadata")
     [message, metadata] = split_string
     metadata_map = Poison.decode!(metadata)
     {message, metadata_map}
