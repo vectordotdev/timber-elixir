d4ba540c9ef5e25ba73dc97a2bb3043a113fc55f
Ben Johnson <bjohnson@binarylogic.com>
2017-02-22 10:31:24 -0600

    Add pool back

--- lib/timber/transports/http/hackney_client.ex
+++ lib/timber/transports/http/hackney_client.ex
@@ -38,10 +38,9 @@ defmodule Timber.Transports.HTTP.HackneyClient do
 
   @doc false
   def start_link do
-    # children = [:hackney_pool.child_spec(@pool_name, get_pool_options())]
-    # opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
-    # Supervisor.start_link(children, opts)
-    {:ok, self()}
+    children = [:hackney_pool.child_spec(@pool_name, get_pool_options())]
+    opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 
   defp config, do: Application.get_env(:timber, :hackney_client, [])
