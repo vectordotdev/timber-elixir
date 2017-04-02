d5a779cce8ca14c7addd32cacfd483b8eed531a0
Ben Johnson <bjohnson@binarylogic.com>
2017-04-02 09:41:25 -0500

    Add test

--- test/lib/timber/event_test.exs
+++ test/lib/timber/event_test.exs
@@ -9,5 +9,11 @@ defmodule Timber.EventTest do
       map = Event.to_api_map(custom_event)
       assert map == %{custom: %{build: %{version: "1.0.0"}}}
     end
+
+    test "controller call event" do
+      event = %Timber.Events.ControllerCallEvent{action: "action", controller: "controller", params_json: "{\"key\": \"value\"}", pipelines: [1]}
+      map = Event.to_api_map(event)
+      assert map == %{server_side_app: %{controller_call: %{action: "action", controller: "controller", params_json: "{\"key\": \"value\"}"}}}
+    end
   end
 end
