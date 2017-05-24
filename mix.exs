7fc75dcd15ed3602785d86a6894df02b734a0e03
Ben Johnson <bjohnson@binarylogic.com>
2017-05-24 12:13:14 -0500

    Release v2.1.8

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Timber.Mixfile do
 
   @source_url "https://github.com/timberio/timber-elixir"
   @homepage_url "https://github.com/timberio/timber-elixir"
-  @version "2.1.7"
+  @version "2.1.8"
 
   # Project manifest for Mix
   #
