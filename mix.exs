2f285a40bf2861ecbc8a9b441d759aa73ece1985
David Antaramian <david@antaramian.com>
2018-10-08 12:55:18 -0400

    Prepare v3.0.0-alpha.3 release

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Timber.Mixfile do
 
   @source_url "https://github.com/timberio/timber-elixir"
   @homepage_url "https://github.com/timberio/timber-elixir"
-  @version "3.0.0-alpha.2"
+  @version "3.0.0-alpha.3"
 
   # Project manifest for Mix
   #
