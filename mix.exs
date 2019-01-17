16a3924908db167c13fc3f0af82bc53a29739a83
Mitchell Henke <mitchh23@gmail.com>
2019-01-17 10:17:57 -0600

    update elixir travis version and excoveralls

--- mix.exs
+++ mix.exs
@@ -199,7 +199,7 @@ defmodule Timber.Mixfile do
       {:earmark, "~> 1.2", only: [:dev]},
       {:ex_doc, "~> 0.19.0", only: [:dev]},
       {:excoveralls, "~> 0.5", only: [:dev, :test]},
-      {:inch_ex, "~> 0.5.6", only: [:dev]}
+      {:inch_ex, "~> 2.0", only: [:dev]}
     ]
   end
 end
